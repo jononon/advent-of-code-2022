d985d3724be8b44294a711128739a13df3d47f1c
jonathan.damico <jonathan.damico@bytedance.com>
2022-12-01 21:55:59 -0800

    feat(day2): add solutions for day 2

--- day2/part2.py
+++ day2/part2.py
@@ -3,26 +3,30 @@
 # BX BY BZ  0  3   6
 # CX CY CZ  6  0   3
 
-# Rock- A or X
-# Paper - B or Y
-# Scissors - C or Z
+# Rock- A or X - 1
+# Paper - B or Y - 2
+# Scissors - C or Z - 3
 
 X += 1
 Y += 2
 Z += 3
 
+X = LOSE = 0
+Y = DRAW = 3
+Z = WIN = 6
+
 '''
 
 scores = {
-    "A X": 3 + 1,
-    "A Y": 6 + 2,
-    "A Z": 0 + 3,
+    "A X": 0 + 3,
+    "A Y": 3 + 1,
+    "A Z": 6 + 2,
     "B X": 0 + 1,
     "B Y": 3 + 2,
     "B Z": 6 + 3,
-    "C X": 6 + 1,
-    "C Y": 0 + 2,
-    "C Z": 3 + 3,
+    "C X": 0 + 2,
+    "C Y": 3 + 3,
+    "C Z": 6 + 1,
 }
 
 def rps():
